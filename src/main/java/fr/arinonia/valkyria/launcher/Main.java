72519ec870e6c73848f24e3bd072aaae1f767044
Arinonia <arinonia.yt@gmail.com>
2020-06-28 04:31:21 +0200

    re create project

---
+++ src/main/java/fr/arinonia/valkyria/launcher/Main.java
@@ -0,0 +1,14 @@
+package fr.arinonia.valkyria.launcher;
+
+/**
+ * Created by Arinonia on 28/06/2020 inside the package - fr.arinonia.valkyria.launcher
+ */
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        //TODO Il est 4h gros je fais juste un repo propre et je vais devant Netflix xD
+    }
+
+}
